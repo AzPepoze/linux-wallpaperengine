64c1cebdea4e0663f2b658989f60e53bb9859519
AzPepoze <65204787+AzPepoze@users.noreply.github.com>
2026-08-22 00:36:35 +0700

    feat(particles): add scene snapshot composition targets

--- src/wallpaper/scene/2d/scene_2d.h
+++ src/wallpaper/scene/2d/scene_2d.h
@@ -2,6 +2,7 @@
 #define SCENE_2D_RUNTIME_H
 
 #include "core/engine_context.h"
+#include "core/gfx_resource.h"
 
 class Scene2DRuntime {
    public:
@@ -10,6 +11,8 @@ class Scene2DRuntime {
     void init();
     void update(float dt);
     void draw();
+    void present();
+    bool requiresOffscreenComposition() const;
     void updateViewport();
     void setOutputViewport(int x, int y, int width, int height);
     void resetOutputViewport();
@@ -17,11 +20,25 @@ class Scene2DRuntime {
     void cleanup();
 
    private:
+    struct SceneTarget {
+        GfxImage image;
+        GfxView texture_view;
+        GfxView attachment_view;
+        int width = 0;
+        int height = 0;
+    };
+
     EngineContext& ctx;
     int output_x = 0;
     int output_y = 0;
     int output_width = 0;
     int output_height = 0;
+    SceneTarget scene_targets[2];
+    int scene_output_index = -1;
+
+    bool ensureSceneTargets(int width, int height);
+    void drawDirect();
+    void drawOffscreen();
 };
 
 #endif  // SCENE_2D_RUNTIME_H
